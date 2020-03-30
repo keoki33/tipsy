0d9e49f7dd1fea80447a9e2d491ea898ac3f9025
keoki33 <gwong323@yahoo.com>
2020-03-31 10:55:06 +1100

    start mvc

---
+++ Tipsy/Models/Tip.swift
@@ -0,0 +1,9 @@
+//
+//  Tip.swift
+//  Tipsy
+//
+//  Created by Keoki on 31/3/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Tipsy/Models/TipBrain.swift
@@ -0,0 +1,11 @@
+//
+//  TipBrain.swift
+//  Tipsy
+//
+//  Created by Keoki on 31/3/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+
